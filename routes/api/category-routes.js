cc50d99c7d80e8cb6a2e02cbac054627cd431067
David Mackey <davidmackey92@gmail.com>
2023-03-18 03:23:16 -0400

    get '/'

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,15 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [ 'id', 'category_name' ],
+    include: [
+      {
+        model: Product,
+        attributes: [ 'id', 'product_name', 'price', 'stock', 'category_id' ]
+      }
+    ]
+  });
 });
 
 router.get('/:id', (req, res) => {
